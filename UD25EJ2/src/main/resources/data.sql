795aaef25187ca5694a07a3a2ff087e6bec74271
carlavelasco15 <carlavelasco15@gmail.com>
2023-11-10 23:51:02 +0100

    added sql, change @controller ti  @restcontroller

---
+++ UD25EJ2/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+-- Inserts for departamentos table
+INSERT INTO departamentos (nombre, presupuesto) VALUES ('Departamento1', 100000);
+INSERT INTO departamentos (nombre, presupuesto) VALUES ('Departamento2', 80000);
+INSERT INTO departamentos (nombre, presupuesto) VALUES ('Departamento3', 120000);
+INSERT INTO departamentos (nombre, presupuesto) VALUES ('Departamento4', 90000);
+INSERT INTO departamentos (nombre, presupuesto) VALUES ('Departamento5', 110000);
+
+-- Inserts for empleados table
+INSERT INTO empleados (dni, nombre, apellidos, departamento_id) VALUES ('123456789A', 'Empleado1', 'Apellido1', 1);
+INSERT INTO empleados (dni, nombre, apellidos, departamento_id) VALUES ('987654321B', 'Empleado2', 'Apellido2', 2);
+INSERT INTO empleados (dni, nombre, apellidos, departamento_id) VALUES ('555555555C', 'Empleado3', 'Apellido3', 3);
+INSERT INTO empleados (dni, nombre, apellidos, departamento_id) VALUES ('111111111D', 'Empleado4', 'Apellido4', 4);
+INSERT INTO empleados (dni, nombre, apellidos, departamento_id) VALUES ('999999999E', 'Empleado5', 'Apellido5', 5);
